fd0a8d95ccb7d7e32ef9d1819b66bbd0f8c77217
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-21 00:56:52 +0530

    Linear Search Done!

--- src/Arrays/Searching/LinearSearch.java
+++ src/Arrays/Searching/LinearSearch.java
@@ -1,4 +1,13 @@
 package Arrays.Searching;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int [] arr = {10,14,52,56,152};
+        int target = 52;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == target){
+                System.out.println("Target found at " + i + " index");
+            }
+        }
+    }
 }
