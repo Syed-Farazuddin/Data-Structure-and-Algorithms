543c824b70529d16d01213e9bf5506e5e140a79f
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-27 13:03:30 +0530

    hashset method for p41 leetcode

--- src/Arrays/problems/P41.java
+++ src/Arrays/problems/P41.java
@@ -1,11 +1,15 @@
 package Arrays.problems;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 // Smallest possible missing number
 public class P41 {
     public static void main(String[] args) {
-        int arr [] = {7,8,9,11,12};
+        int arr [] = {7,2,1,8,9,11,12};
+        System.out.println(hashset(arr));;
+    }
+    private static int bruteForce(int [] arr){
         Arrays.sort(arr);
         int smallest = 1;
         for(int i =0 ;i < arr.length ; i++){
@@ -13,6 +17,19 @@ public class P41 {
                 smallest++;
             }
         }
-        System.out.println(smallest);
+       return smallest;
+    }
+    private static int hashset(int [] arr){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0 ; i < arr.length ; i++){
+            set.add(arr[i]);
+        }
+        int smallest = 1;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(set.contains(smallest)){
+                smallest += 1;
+            }
+        }
+        return smallest;
     }
 }
