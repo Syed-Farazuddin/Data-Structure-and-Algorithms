3e9133b96e3b64be30c9a60d9559ae399ed29bc9
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-28 14:17:07 +0530

    linked list implemented

---
+++ src/Arrays/LinkedList/LinkList.java
@@ -0,0 +1,36 @@
+package Arrays.LinkedList;
+//Linked list custom implementation
+public class LinkList {
+
+    public Node head;
+    private Node tail;
+    private int size;
+    LinkList(){
+        this.size = 0;
+    }
+
+    private class Node{
+        Node(int value){
+            this.val = value;
+            this.next = null;
+        }
+        Node next;
+        int val;
+    }
+    public void addAtFirst(int value){
+        Node node = new Node(value);
+        if(head == null && tail == null){
+            head = node;
+        }else{
+            node.next = head;
+            head = node;
+        }
+    }
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+    }
+}
---
+++ src/Arrays/LinkedList/Main.java
@@ -0,0 +1,11 @@
+package Arrays.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkList list = new LinkList();
+        list.addAtFirst(10);
+        list.addAtFirst(12);
+        list.addAtFirst(14);
+        list.display();
+    }
+}
