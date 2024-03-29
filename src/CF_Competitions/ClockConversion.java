cfc50c3e5bc3f443bdb2ef3aedd0bd9efcd18c0f
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-29 11:47:47 +0530

    clock conversion changes

--- src/CF_Competitions/ClockConversion.java
+++ src/CF_Competitions/ClockConversion.java
@@ -6,26 +6,24 @@ public class ClockConversion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cases = sc.nextInt();
-        for(int i =0 ; i < cases ; i++){
-            String clock = sc.nextLine();
-            int ptr = 0;
-            int hour = 0;
-            String res = "";
-            while(ptr < clock.length()-1){
-                if(ptr < 2){
-                    hour = hour*10 + ((int)clock.charAt(ptr)-48);
-                }
-                ptr++;
-                if(ptr > 2){
-                    if( hour > 12){
-                        res = "" + (hour - 12);
-                    }else{
-                        res = "" + hour;
-                    }
-                    res += clock.charAt(ptr);
-                }
+        while(cases > 0){
+            int f =0;
+            String i = sc.next();
+            int h = (i.charAt(0)- '0')*10 + (i.charAt(1) - '0');
+            if(h > 12) {
+                h -= 12;
+                f=1;
             }
-            System.out.println(res);
+            String hh = h + "";
+            if(hh.length() == 1){
+                hh = "0" + hh;
+            }
+            if(f == 1){
+                System.out.println(hh + ":" + i.charAt(3) + i.charAt(4)+" PM");
+            }else{
+                System.out.println(hh+":"+i.charAt(3)+i.charAt(4)+" AM");
+            }
+            cases--;
         }
     }
 }
--- src/CF_Competitions/StairOrPeak.java
+++ src/CF_Competitions/StairOrPeak.java
@@ -7,13 +7,14 @@ public class StairOrPeak {
         Scanner sc = new Scanner(System.in);
         int cases = sc.nextInt();
         while(cases > 0){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            if(a < b && b<c){
+            int arr [] = new int[3];
+            for(int i =0 ; i < 3 ; i++){
+                arr[i] = sc.nextInt();
+            }
+            if(arr[0] < arr[1] && arr[1] < arr[2]){
                 System.out.print("STAIR\n");
             }
-            else if(a<b && b>c){
+            else if(arr[0]<arr[1] && arr[1]>arr[2]){
                 System.out.print("PEAK\n");
             }else{
                 System.out.print("NONE\n");
