9a08bb4415a551e0840d1096bf60502c468a3151
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-29 14:18:34 +0530

    hashmpa iteration methods

--- src/HashMaps/functions/Iteration.java
+++ src/HashMaps/functions/Iteration.java
@@ -2,6 +2,7 @@ package HashMaps.functions;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Iteration {
     public static void main(String[] args) {
@@ -11,6 +12,13 @@ public class Iteration {
         map.put(52,1413);
         for(Map.Entry<Integer,Integer> e : map.entrySet()){
             System.out.println(e.getKey() + " : " + e.getValue());
+        }
+
+//        Method 2 of iteration
+
+        Set<Integer> set =map.keySet();
+        for(int key : set){
+            System.out.println(key + " : " + map.get(key));
         }
     }
 }
