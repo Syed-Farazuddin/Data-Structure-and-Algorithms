6cf398fc0591a4d9504fe61e9043aaa7c0883193
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-31 07:22:19 +0530

    flkadsjf

--- src/SlidingWindow/SubarraySumEqualtoK.java
+++ src/SlidingWindow/SubarraySumEqualtoK.java
@@ -2,6 +2,21 @@ package SlidingWindow;
 
 public class SubarraySumEqualtoK {
     public static void main(String[] args) {
-
+        System.out.println(subarraySum(new int[] {1,2,3}, 2)-subarraySum(new int[] {1,2,3} , 2-1));
+    }
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0,left = 0, right = 0, sum = 0;
+        while(right < nums.length){
+            sum += nums[right];
+            if(sum == k){
+                count = right - left + 1;
+            }
+            while(sum > k){
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return count;
     }
 }
