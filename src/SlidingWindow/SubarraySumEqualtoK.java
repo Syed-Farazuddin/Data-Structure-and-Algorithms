9f7c474fb39f46ca6fe70858a316224471f26a5a
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-31 05:48:00 +0530

    shuhhhhhhhhhhh

---
+++ src/SlidingWindow/SubarraySumEqualtoK.java
@@ -0,0 +1,7 @@
+package SlidingWindow;
+
+public class SubarraySumEqualtoK {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Strings/problems/MaxOddBinaryNumber.java
@@ -0,0 +1,12 @@
+package Strings.problems;
+
+public class MaxOddBinaryNumber {
+    public static void main(String[] args) {
+        System.out.println(maximumOddBinaryNumber("010"));
+    }
+    public static String maximumOddBinaryNumber(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        return ans.toString();
+    }
+}
