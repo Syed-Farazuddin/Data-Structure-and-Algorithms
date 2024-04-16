ae7542be5208d241e00c42bd051d72aabec469fb
Syed Faraz <syedfarazuddin73@gmail.com>
2024-04-16 13:52:32 +0530

    Recursion intermediate started

---
+++ src/Recursion/Intermediate/MultipleFunctionCalls.java
@@ -0,0 +1,7 @@
+package Recursion.Intermediate;
+
+public class MultipleFunctionCalls {
+    public static void main(String[] args) {
+        System.out.println("MULTIPLE FUNCTION CALLS");
+    }
+}
