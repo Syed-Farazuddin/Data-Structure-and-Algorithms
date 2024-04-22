898b96bfa58be94704d5457a6478417cebe7ccd3
Syed Faraz <syedfarazuddin73@gmail.com>
2024-04-22 22:18:45 +0530

    mhgf

---
+++ src/SecondONe.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SecondONe {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int cases = sc.nextInt();
+//        for (int i = 0; i < cases; i++) {
+//            int n = sc.nextInt(), k = sc.nextInt();
+//            while(n != 0 && k != 0){
+//                int c = k/n;
+//                if(c != 0) System.out.print(c + " ");
+//                k = k - c;
+//                n = n-1;
+//            }
+//            System.out.println();
+        findOr();
+        }
+    private static void findOr(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = 0;
+        for(int i = 0 ; i < n ; i++){
+            int curr = sc.nextInt();
+            res = res | curr;
+        }
+        System.out.println(res);
+    }
+}
--- src/checks.java
+++ src/checks.java
@@ -1,7 +1,35 @@
-import java.util.*;
-public class checks {
-        public static void main(String [] args) {
-            Scanner sc = new Scanner(System.in);
+// 5
+// -5 0 -3 -4 12
 
+//Even: 3
+//Odd: 2
+//Positive: 1
+//Negative: 3
+import java.util.Scanner;
+
+public class checks{
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int even = 0, odd = 0, positive = 0 , negative = 0;
+        for(int i = 1; i <= n ; i++){
+            int curr = in.nextInt();
+            if(curr > 0){
+                positive++;
+            }
+            if(curr < 0){
+                negative++;
+            }
+            if(curr%2 == 0){
+                even++;
+            }
+            if(curr%2 != 0){
+                odd++;
+            }
         }
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+        System.out.println("Positive: " + positive);
+        System.out.println("Negative: " + negative);
+    }
 }
